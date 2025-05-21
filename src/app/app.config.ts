ec0c54092c27842f5a0e50de1a57a2ab8ce1a032
pk404849 <pk404849@gmail.com>
2025-05-21 23:05:53 +0530

    reverse commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient,withFetch} from '@angular/common/http';
-//import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimations } from '@angular/platform-browser/animations';
     
 export const appConfig: ApplicationConfig = {
   
@@ -12,7 +12,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
      provideClientHydration(withEventReplay()),
      provideHttpClient(withFetch()),
-     //provideAnimations(),
+     provideAnimations(),
     
 
   ]
