27b6ad59c944c72c14990ddd9e4ee51ad17de429
pk404849 <pk404849@gmail.com>
2025-06-07 19:48:21 +0530

    enable public ip

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
- //baseUrl = 'http://13.53.206.50:3000';
-  baseUrl : 'http://172.31.37.139:3000',
+ baseUrl : 'http://13.53.206.50:3000',
+  //baseUrl : 'http://172.31.37.139:3000',
   //baseUrl : 'http://localhost:3000',
 
-  //websocketserverUrl : 'http://13.53.206.50:3000/ws-option-chain'
-  websocketserverUrl : 'http://172.31.37.139:3000/ws-option-chain'
+  websocketserverUrl : 'http://13.53.206.50:3000/ws-option-chain'
+  //websocketserverUrl : 'http://172.31.37.139:3000/ws-option-chain'
   //websocketserverUrl : 'http://localhost:3000/ws-option-chain'
 };
