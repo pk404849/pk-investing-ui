d393ce3897fe2a70f7800d26060f32bbfcd0f95b
pk404849 <pk404849@gmail.com>
2025-06-07 19:56:07 +0530

    test

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
- baseUrl : 'http://13.53.206.50:3000',
-  //baseUrl : 'http://172.31.37.139:3000',
+ //baseUrl : 'http://13.53.206.50:3000',
+  baseUrl : 'http://172.31.37.139:3000',
   //baseUrl : 'http://localhost:3000',
 
   websocketserverUrl : 'http://13.53.206.50:3000/ws-option-chain'
